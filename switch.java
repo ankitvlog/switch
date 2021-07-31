1903dd466224d538459501cf35810bdd6c704159
ankitvlog <88242036+ankitvlog@users.noreply.github.com>
2021-07-31 21:48:28 +0530

    created switch.java
    This file prints switch program on the screen.

---
+++ switch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Switchcase{
+public static void main(String[] arge){
+int choice;
+System.out.println("Pick one : 1. Hi\t2. Hey\t3. hello\t");
+Scanner s = New Scanner(System.in);
+choice = s.nextInt();
+switch(choice)
+{
+case 1 : System.out.println("You said Hi");
+break;
+case 2 : System.out.println("You said Hey");
+break;
+case 3 : System.out.println("You said hello");
+break;
+default :  System.out.println("Invalid choice");
+}
+}
+}
